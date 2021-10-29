79f4711719a5df509a0e8d84696a10ebc0a8e029
Nyviii70 <bwb.georges.l@fondespierre.com>
2021-10-29 12:20:21 +0000

    checkpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AddPage } from './add/add.page';
 
 const routes: Routes = [
   {
@@ -11,9 +12,12 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    // on dit qu'à l'écriture de 'add'
+    path: 'add',
+    // on appelle ce component
+   component: AddPage
   },
 ];
 
